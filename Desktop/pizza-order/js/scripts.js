d9a1cfd7472b806bf62eeeb6fafe76ce16fa615d
Meredith <alcornmeredith@gmail.com>
2016-09-01 15:39:41 -0400

    take out extra properties, move business logic to top of document, made test pizza

--- Desktop/pizza-order/js/scripts.js
+++ Desktop/pizza-order/js/scripts.js
@@ -1,4 +1,24 @@
-$(document).ready(function(){
+//Constructor for pizza object - may have too many properties
+  function Pizza(sizeParameter){
+    this.size = sizeParameter;
+    this.toppings = [];
+    this.price = 0;
+  }
+
+//Creates a method to calculate the price of sm, md, or lg pizza
+    Pizza.prototype.findPrice = function(sizeResult){
+      if(this.size==='small'){
+        this.price += 9;
+      }
+      else if(this.size === 'medium'){
+        this.price += 11;
+      }
+      else{
+        this.price += 13;
+      }
+    }
+
+  $(document).ready(function(){
   $("form#pizza-order").submit(function(event){
   event.preventDefault();
 
@@ -6,41 +26,26 @@ $(document).ready(function(){
   (console.log(sizeSelection))
 //TODO: Grab values from checkboxes.  Below isn't working:
   var toppingSelection = document.getElementsByName("topping");
-  var i;
-  for (i = 0; i < toppingSelection.length; i++) {
-    if (toppingSelection[i].type == "checkbox") {
-        toppingSelection[i].checked = true;
-    }
-}
-  (console.log(toppingSelection))
+  for (var checkbox = 0; checkbox < toppingSelection.length; checkbox++) {
+    console.log(toppingSelection[checkbox].checked);
+    console.log(toppingSelection[checkbox].type);
+    console.log(toppingSelection[checkbox].value);
 
-
-//Constructor for pizza object - may have too many properties
-  function Pizza(size){
-    this.size = size;
-    this.sizePrice = 0
-    this.topping = true || false;
-    this.toppingPrice = true || false;
   }
+  console.log(toppingSelection)
 
-//Creates a method to calculate the price of sm, md, or lg pizza
-    Pizza.prototype.sizeTotal = function(sizeResult){
-      if(this.size==='small'){
-        this.sizePrice += 9;
-      }
-      else if(this.size === 'medium'){
-        this.sizePrice += 11;
-      }
-      else{
-        this.sizePrice += 13;
-      }
-    }
+
+
   //Can't successfully log a price
-    (console.log(this.sizePrice))
+    console.log(this.sizePrice)
 
 
+var pizzaObject = new Pizza("small");
+pizzaObject.toppings.push("ham", "pineapple");
 
-
+var largePizza = new Pizza("large");
+largePizza.toppings.push("chicken");
+console.log(pizzaObject, largePizza);
 
 
 
